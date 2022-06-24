06a209b73afaf0c15ea354feed378c56cc638c45
Felix Seemann <fseemann@mail.de>
2022-06-24 14:31:53 +0200

    Fix typos

--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,50 @@
 <template>
-      <div class="py-16">
-        <header>
-          <h1 class="font-bold text-4xl leading-relaxed md:text-6xl md:leading-loose text-slate-100">Felix Seemann 👋</h1>
-          <p class="text-lg leading-relaxed md:text-xl md:leading-loose">
-            <span class="text-slate-100 font-bold">Full-Stack Developer</span> und
-            <span class="text-slate-100 font-bold">Domain-Driven Design Spezialist</span>
-            mit 7+ Jahren Berufserfahrung.</p>
-        </header>
-        <main class="text-lg leading-relaxed md:text-xl md:leading-loose space-y-16 mt-16">
-          <p>
-            Ich schreibe webbasierte Geschäftsanwendung in Kotlin / Java und Vue. Dabei wende ich technische Pattern aus
-            Domain-Driven Design an, um Geschäftslogik von technischen Abhängigkeiten zu befreien und verschiede
-            Anwendungsfälle lose über Events zu verbinden. Dabei teste ich die Anwendungsfälle über qualitative
-            Integrationstests.
-          </p>
-          <p>
-            Mir ist es wichtig einfache Lösungen zu produzieren und nehme ich mir vorab Zeit über ein gutes Konzept
-            nachzudenken. Ich vermeide deswegen magische Frameworks, die mir die Kontrolle über den wichtigsten Teil
-            meiner Anwendung entreißen (JPA 😡).
-          </p>
-          <p>
-            Zuletzt habe ich mit Ktor, Spring, JakartaEE, REST, GraphQL, MongoDB, MySQL, RabbitMQ, Vue, Nuxt und
-            Tailwind
-            gearbeitet.
-          </p>
-          <p>
-            Bei der CodeFabrik GmbH, meinem jüngsten Arbeitergeber, konnte ich an zwei öffentlichen (neben weiteren
-            internen) Projekten arbeiten. Schaut euch gerne <a href="https://github.com/CodeFabrikGmbH/scenarios-kotlin"
-                                                               target="_blank" class="underline">scenarios-kotlin</a>,
-            eine
-            Bibliothek für lesbare
-            Integrationstests, und <a href="https://www.zeitfabrik.de/" target="_blank" class="underline">ZeitFabrik</a>,
-            ein Produkt zur Zeiterfassung und
-            Projektabrechnung, an. Auch einen
-            <a href="https://www.code-fabrik.com/2021/03/10/the-definitive-guide-to-software-architecture"
-               target="_blank" class="underline">Blogeintrag über Softwarearchitektur</a> habe ich geschrieben. 🎉
-          </p>
-          <p>
-            Ich unterstütze Ihr Team und teile mein Wissen über die Entwicklung wartbarer Software, indem ich eine
-            mehrschichtige Softwarearchitektur und Aggregat Designprinzipien mit loser Kopplung über Domain-Events
-            anwende.
-          </p>
-          <div>
-            <a class="inline-block px-4 py-2 md:px-6 md:py-4 border border-blue-700 bg-blue-700 text-blue-100 rounded rounded-md font-bold tracking-wide"
-               href="mailto:fseemann@mail.de">
-              Kontakt aufnehmen
-            </a>
-          </div>
-        </main>
+  <div class="py-16">
+    <header>
+      <h1 class="font-bold text-4xl leading-relaxed md:text-6xl md:leading-loose text-slate-100">Felix Seemann 👋</h1>
+      <p class="text-lg leading-relaxed md:text-xl md:leading-loose">
+        <span class="text-slate-100 font-bold">Full-Stack Developer</span> und
+        <span class="text-slate-100 font-bold">Domain-Driven Design Spezialist</span>
+        mit 7+ Jahren Berufserfahrung.</p>
+    </header>
+    <main class="text-lg leading-relaxed md:text-xl md:leading-loose space-y-16 mt-16">
+      <p>
+        Ich schreibe webbasierte Geschäftsanwendung in Kotlin / Java und Vue. Dabei wende ich technische Pattern aus
+        Domain-Driven Design an, um Geschäftslogik von technischen Abhängigkeiten zu befreien und verschiede
+        Anwendungsfälle lose über Events zu verbinden. Dabei teste ich die Anwendungsfälle über qualitative
+        Integrationstests.
+      </p>
+      <p>
+        Mir ist es wichtig einfache Lösungen zu produzieren und nehme mir vorab Zeit über ein gutes Konzept
+        nachzudenken. Ich vermeide deswegen magische Frameworks, die mir die Kontrolle über den wichtigsten Teil
+        meiner Anwendung entreißen (JPA 😡).
+      </p>
+      <p>
+        Zuletzt habe ich mit Ktor, Spring, JakartaEE, REST, GraphQL, MongoDB, MySQL, RabbitMQ, Vue, Nuxt und
+        Tailwind
+        gearbeitet.
+      </p>
+      <p>
+        Bei der CodeFabrik GmbH, meinem jüngsten Arbeitergeber, konnte ich an zwei öffentlichen Projekten (neben
+        weiteren internen) arbeiten. Schaut euch gerne <a href="https://github.com/CodeFabrikGmbH/scenarios-kotlin"
+                                                          target="_blank" class="underline">scenarios-kotlin</a>,
+        eine
+        Bibliothek für lesbare
+        Integrationstests, und <a href="https://www.zeitfabrik.de/" target="_blank" class="underline">ZeitFabrik</a>,
+        ein Produkt zur Zeiterfassung und
+        Projektabrechnung, an. Auch einen
+        <a href="https://www.code-fabrik.com/2021/03/10/the-definitive-guide-to-software-architecture"
+           target="_blank" class="underline">Blogeintrag über Softwarearchitektur</a> habe ich geschrieben. 🎉
+      </p>
+      <p>
+        Gerne unterstütze ich Ihr Team und teile mein Wissen über die Entwicklung wartbarer Software.
+      </p>
+      <div>
+        <a class="inline-block px-4 py-2 md:px-6 md:py-4 border border-blue-700 bg-blue-700 text-blue-100 rounded rounded-md font-bold tracking-wide"
+           href="mailto:fseemann@mail.de">
+          Kontakt aufnehmen
+        </a>
       </div>
+    </main>
+  </div>
 </template>
