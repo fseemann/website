20b29ff3c5d78864afa93aea3602717482a7c57e
Felix Seemann <fseemann@mail.de>
2022-11-07 13:44:58 +0100

    Add section for each layer

--- pages/domain-driven-design/index.vue
+++ pages/domain-driven-design/index.vue
@@ -13,7 +13,9 @@ onMounted(() => Prism.highlightAll())
         Domain-Driven Design. You can traverse through the code by
         clicking on methods and read the explanations provided by code documentation or pop-overs.</p>
     </div>
-    <div>
+    <div class='px-4 py-16 bg-yellow-400'>
+      <h3 class='inline-block -skew-y-3 uppercase text-yellow-50 font-bold text-4xl'>Infrastructure</h3>
+      <div class='mt-2 grid grid-rows-2 md:grid-rows-none md:grid-cols-2 gap-4'>
       <pre>
 <code class='language-kotlin'>
 class TimeRecordController {
@@ -24,6 +26,14 @@ class TimeRecordController {
 }
 </code>
       </pre>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aperiam dolorem eaque, expedita ipsa ipsum nam possimus quidem vero voluptates.</p>
+      </div>
+    </div>
+    <div class='px-4 py-16 bg-blue-400'>
+      <h3 class='inline-block -skew-y-3 uppercase text-blue-50 font-bold text-4xl'>Application</h3>
+    </div>
+    <div class='px-4 py-16 bg-green-400'>
+      <h3 class='inline-block -skew-y-3 uppercase text-green-50 font-bold text-4xl'>Domain</h3>
     </div>
   </main>
 </template>
