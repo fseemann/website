0a8cae16f131c9e4218df74219238264d3de7d44
Felix Seemann <fseemann@mail.de>
2022-12-23 13:18:47 +0100

    Update to nuxt to release version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,9 @@
-import { defineNuxtConfig } from 'nuxt'
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  build: {
-    postcss: {
-      postcssOptions: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
-    },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
   },
-  css: ['~/assets/tailwind.css'],
-})
+  css: ["~/assets/tailwind.css"]
+});
