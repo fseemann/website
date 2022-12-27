a3a235f90a3d19283bd42b558e6294a0346be717
Felix Seemann <fseemann@mail.de>
2022-12-27 14:41:16 +0100

    Add mobile first navigation element

--- app.vue
+++ app.vue
@@ -4,15 +4,21 @@
       class="min-h-screen mx-auto max-w-screen-lg px-8 py-8 text-stone-800 dark:text-slate-100 antialiased font-sans leading-snug flex flex-col"
     >
       <header>
-        <nav class="flex justify-between">
-          <div>
-            <NuxtLink class="font-bold text-xl sm:text-2xl" to="/">Felix Seemann</NuxtLink>
+        <nav>
+          <div class="flex justify-between">
+            <div>
+              <NuxtLink class="font-bold text-xl sm:text-2xl" to="/">Felix Seemann</NuxtLink>
+            </div>
+            <div class="flex items-center space-x-4 sm:space-x-8">
+              <NuxtLink class="invisible sm:visible" to="/blog">Blog</NuxtLink>
+              <input-toggle v-model="darkMode">
+                <svg-moon class="w-4" />
+              </input-toggle>
+            </div>
           </div>
-          <div class="flex items-center space-x-4 sm:space-x-8">
-            <NuxtLink to="/blog">Blog</NuxtLink>
-            <input-toggle v-model="darkMode">
-              <svg-moon class="w-4" />
-            </input-toggle>
+          <div class="mt-4 sm:hidden space-x-4">
+            <NuxtLink class="underline" to="/">Home</NuxtLink>
+            <NuxtLink class="underline" to="/blog">Blog</NuxtLink>
           </div>
         </nav>
       </header>
@@ -49,19 +55,19 @@ useHead({
     { name: "author", content: "Felix Seemann" }
   ],
   script: [
-    { src: 'https://platform.twitter.com/widgets.js', charSet: 'utf-8' }
+    { src: "https://platform.twitter.com/widgets.js", charSet: "utf-8" }
   ]
 });
 
 const colorMode = useColorMode();
 
 const darkMode = computed({
-  get () {
-    return colorMode.preference === "dark"
+  get() {
+    return colorMode.preference === "dark";
   },
-  set (newValue) {
-    colorMode.preference = newValue ? "dark" : "light"
+  set(newValue) {
+    colorMode.preference = newValue ? "dark" : "light";
   }
-})
+});
 
 </script>
