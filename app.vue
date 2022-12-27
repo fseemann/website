aedaf3351709a87df17d62803b5a547dd9db0f09
Felix Seemann <fseemann@mail.de>
2022-12-27 10:54:18 +0100

    Extract toggle component

--- app.vue
+++ app.vue
@@ -10,11 +10,9 @@
               <NuxtLink class="font-bold text-2xl" to="/">Felix Seemann</NuxtLink>
             </div>
             <div class="flex items-center space-x-8">
-              <label class="inline-flex relative items-center cursor-pointer">
-                <input type="checkbox" value="" class="sr-only peer" v-model="state.darkMode">
-                <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                <span class="ml-2"><svg-moon class="w-4"/></span>
-              </label>
+              <input-toggle v-model="state.darkMode">
+                <svg-moon class="w-4"/>
+              </input-toggle>
             </div>
           </nav>
         </header>
