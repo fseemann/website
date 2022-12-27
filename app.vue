b5f9edce2684ff689a7927618e925c0c41bc61e3
Felix Seemann <fseemann@mail.de>
2022-12-27 10:48:13 +0100

    Add toggle

--- app.vue
+++ app.vue
@@ -9,10 +9,12 @@
             <div>
               <NuxtLink class="font-bold text-2xl" to="/">Felix Seemann</NuxtLink>
             </div>
-            <div class="flex items-start space-x-6">
-              <input type="checkbox" v-model="state.darkMode">
-              <NuxtLink class="font-medium" to="/blog">Blog</NuxtLink>
-              <a href="mailto:fseemann@mail.de" class="font-medium">Contact</a>
+            <div class="flex items-center space-x-8">
+              <label class="inline-flex relative items-center cursor-pointer">
+                <input type="checkbox" value="" class="sr-only peer" v-model="state.darkMode">
+                <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                <span class="ml-2"><svg-moon class="w-4"/></span>
+              </label>
             </div>
           </nav>
         </header>
