2e7b8cc4bb1ccdcbcd4c03e47eb360a0f85e297b
Felix Seemann <fseemann@mail.de>
2022-12-28 12:25:58 +0100

    Remove bug where article is duplicated

--- pages/blog/[path].vue
+++ pages/blog/[path].vue
@@ -1,9 +1,11 @@
 <template>
   <main class="py-16">
     <article>
-      <ContentDoc :path="route.path" v-slot="{doc}">
+      <ContentDoc :path="path" v-slot="{doc}">
         <p class="text-neutral-600 dark:text-neutral-200">{{ dateFormat.format(doc.writtenAt) }}</p>
-        <ContentRenderer class="mt-4 prose prose-pre:bg-slate-50 dark:prose-pre:bg-neutral-800 dark:prose-invert sm:prose-lg lg:prose-xl" :value="doc" />
+        <ContentRenderer
+          class="mt-4 prose prose-pre:bg-slate-50 dark:prose-pre:bg-neutral-800 dark:prose-invert sm:prose-lg lg:prose-xl"
+          :value="doc" />
       </ContentDoc>
     </article>
   </main>
@@ -14,4 +16,7 @@ import { useDateFormat } from "~/shared/useDateFormat";
 
 const route = useRoute();
 const dateFormat = useDateFormat();
+
+const path = route.path.charAt(route.path.length - 1) === "/" ? route.path.substr(0, route.path.length - 1) : route.path;
+
 </script>
